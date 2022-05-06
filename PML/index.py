a7c68cc1c90d309c395131eb638ea82727dedb2d
MaxCardenasCasqui <73789439@continental.edu.pe>
2022-05-06 01:20:03 -0500

    Modificaciones de la opción "Familia" para guardar imagenes de usuarios

--- PML/index.py
+++ PML/index.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.config["UPLOAD_FOLDER"] = "static/uploads"
+ALLOWED_EXTENSIONS = set(['png', "jpg", "jpeg"])
 
 @app.route('/')
-def Home():
+def home():
     return render_template('home.html')
 
 @app.route('/camera')
-def Camera():
+def camera():
     return render_template('camera.html')
 
-@app.route('/templates/family')
-def Family():
+@app.route('/family')
+def family():
     return render_template('family.html')
 
+def allowed_file(file):
+    file = file.split('.')
+    if file[1] in ALLOWED_EXTENSIONS:
+        return True
+    return False
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    file = request.files["uploadFile"]
+    print(file, file.filename)
+    filename = secure_filename(file.filename)
+    print(filename)
+    if file and allowed_file(filename):
+        print("permitido")
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return 'A'
+
 @app.route('/recording')
-def Recording():
+def recording():
     return render_template('recording.html')
 
 if __name__ == '__main__':
